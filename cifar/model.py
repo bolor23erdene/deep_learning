1ba4380ae5b0a11d0ea69a647139c5524c4f71e9
bzolbayar <bzolbayar@ebay.com>
2022-04-26 18:04:29 -0700

    decrease the size of the kernel_size

--- cifar/model.py
+++ cifar/model.py
@@ -14,14 +14,14 @@ class DoubleCnnThreeMlp(nn.Module):
         self.pool = nn.MaxPool2d(2, 2) # 15
         self.conv2 = nn.Conv2d(6, 16, 3) # 13
 
-        self.fc1 = nn.Linear(13 * 13 * 16, 120)
+        self.fc1 = nn.Linear(6 * 6 * 16, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 13 * 13 * 16)
+        x = self.pool(F.relu(self.conv2(x))) #6x6x16
+        x = x.view(-1, 6 * 6 * 16)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)

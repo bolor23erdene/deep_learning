80065ec4dd25cda5140fe1b6a79ee0d04dbf2958
bzolbayar <bzolbayar@ebay.com>
2022-04-26 17:57:49 -0700

    double cnn three mlp

--- cifar/model.py
+++ cifar/model.py
@@ -9,7 +9,7 @@ import torch
 
 class DoubleCnnThreeMlp(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(DoubleCnnThreeMlp, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)

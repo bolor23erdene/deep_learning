16a752630085699b912f87adc32190c0f7c603dd
bzolbayar <bzolbayar@ebay.com>
2022-04-26 18:42:34 -0700

    check evaluation

--- mnist/classifier.py
+++ mnist/classifier.py
@@ -63,7 +63,7 @@ for r in range(nb_epochs):
             print("Loss: ", total_loss/total_cnt)
             print(images.shape)
             
-    loss.append([i, total_loss.item()])
+    #loss.append([i, total_loss.item()])
 
 print('Finished Training')
 
@@ -77,56 +77,56 @@ images, labels = dataiter.next()
 
 # print images
 # imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+#print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
-# """
-# Evaluation
-# """
-# PATH = './mnist_net.pth'
+"""
+Evaluation
+"""
+PATH = './mnist_net.pth'
 
-# transform = transforms.Compose(
-#     [transforms.ToTensor(),
-#      transforms.Normalize((0.1307,), (0.3081,))])
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.1307,), (0.3081,))])
 
-# testset = torchvision.datasets.CIFAR10(
-#     root='./data',
-#     train=False,
-#     download=True,
-#     transform=transform)
+testset = torchvision.datasets.CIFAR10(
+    root='./data',
+    train=False,
+    download=True,
+    transform=transform)
 
-# testloader = torch.utils.data.DataLoader(
-#     testset, batch_size=4, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=4, shuffle=False, num_workers=2)
 
-# classes = ('0', '1')
+classes = ('0', '1')
 
-# dataiter = iter(testloader)
-# images, labels = dataiter.next()
-# net = DoubleCnnThreeMlp()
-# net.load_state_dict(torch.load(PATH))
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+net = DoubleCnnThreeMlp()
+net.load_state_dict(torch.load(PATH))
 
-# outputs = net(images)
+outputs = net(images)
 
-# _, predicted = torch.max(outputs, 1)
+_, predicted = torch.max(outputs, 1)
 
-# print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
 
-# """
-# Accuracy
-# """
-# correct = 0
-# total = 0
-# with torch.no_grad():
-#     for data in testloader:
-#         images, labels = data
-#         outputs = net(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
+"""
+Accuracy
+"""
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-# print('Accuracy of the network on the 10000 test images: %d %%' % (
-#     100 * correct / total))
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
 
 
 

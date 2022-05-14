40d69e3a2d86fc3882ac491e290b1e038a09fd69
bzolbayar <bzolbayar@ebay.com>
2022-05-14 11:37:40 -0700

    lstm att WIP

--- lstm/model.py
+++ lstm/model.py
@@ -109,9 +109,9 @@ class LSTM_ATTN(nn.Module):
     Below is a visual representation of what squeeze/unsqueeze do for an 2D matrix.
     """
     def attention(self, lstm_output, final_state):
-        print("lstm_output: ", lstm_output.shape)
-        print("final_state: ", final_state.shape)
-        lstm_output = lstm_output.permute(1, 0, 2) # (batch, seq_len, hid)-> (seq_len, batch, hid)
+        print("lstm_output: ", len(lstm_output))
+        print("final_state: ", len(final_state))
+        #lstm_output = lstm_output.permute(1, 0, 2) # (batch, seq_len, hid)-> (seq_len, batch, hid)
         
         # final_state= (1,batch,hidden)
         merged_state = torch.cat([s for s in final_state], 1) # (seq_len, hid)
@@ -119,8 +119,8 @@ class LSTM_ATTN(nn.Module):
         # squeeze=Returns a tensor with all the dimensions of input of size 1 removed.
         # merged_state.squeeze(0) -> (batch,hidden)
         # merged_state.squeeze(0).unsqueeze(2) -> (batch,hidden,1)
-        merged_state = merged_state.squeeze(0).unsqueeze(2) 
-        weights = torch.bmm(lstm_output, merged_state) # batch, seq_len, hidden x batch, hidden, 1 
+        #merged_state = merged_state.squeeze(0).unsqueeze(2) 
+        weights = torch.bmm(lstm_output, final_state) # batch, seq_len, hidden x batch, hidden, 1 
         weights = F.softmax(weights.squeeze(2), dim=1).unsqueeze(2) # batch x seq_len
         return torch.bmm(torch.transpose(lstm_output, 1, 2), weights).squeeze(2)
     

35793f3adb026d584661fcd96d0b14ecb986d910
bzolbayar <bzolbayar@ebay.com>
2022-06-18 14:46:27 -0700

    different v

--- lstm/nmt.py
+++ lstm/nmt.py
@@ -19,50 +19,55 @@ from torch.utils.tensorboard import SummaryWriter
 1. download the english and german language
 2. create tokenizer 
 """
-spacy_german = spacy.load("de_core_news_sm")
-spacy_english = spacy.load("en_core_news_sm")
+import torchtext
+import torch
+from torchtext.data.utils import get_tokenizer
+from collections import Counter
+from torchtext.vocab import Vocab
+from torchtext.utils import download_from_url, extract_archive
+import io
 
-def tokenize_german(text):
-    return [token.text for token in spacy_german.tokenizer(text)]
+url_base = 'https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/'
+train_urls = ('train.de.gz', 'train.en.gz')
+val_urls = ('val.de.gz', 'val.en.gz')
+test_urls = ('test_2016_flickr.de.gz', 'test_2016_flickr.en.gz')
 
-def tokenize_english(text):
-    return [token.text for token in spacy_english.tokenizer(text)]
+train_filepaths = [extract_archive(download_from_url(url_base + url))[0] for url in train_urls]
+val_filepaths = [extract_archive(download_from_url(url_base + url))[0] for url in val_urls]
+test_filepaths = [extract_archive(download_from_url(url_base + url))[0] for url in test_urls]
+
+de_tokenizer = get_tokenizer('spacy', language='de')
+en_tokenizer = get_tokenizer('spacy', language='en')
+
+def build_vocab(filepath, tokenizer):
+  counter = Counter()
+  with io.open(filepath, encoding="utf8") as f:
+    for string_ in f:
+      counter.update(tokenizer(string_))
+  return Vocab(counter, specials=['<unk>', '<pad>', '<bos>', '<eos>'])
+
+de_vocab = build_vocab(train_filepaths[0], de_tokenizer)
+en_vocab = build_vocab(train_filepaths[1], en_tokenizer)
+
+def data_process(filepaths):
+  raw_de_iter = iter(io.open(filepaths[0], encoding="utf8"))
+  raw_en_iter = iter(io.open(filepaths[1], encoding="utf8"))
+  data = []
+  for (raw_de, raw_en) in zip(raw_de_iter, raw_en_iter):
+    de_tensor_ = torch.tensor([de_vocab[token] for token in de_tokenizer(raw_de)],
+                            dtype=torch.long)
+    en_tensor_ = torch.tensor([en_vocab[token] for token in en_tokenizer(raw_en)],
+                            dtype=torch.long)
+    data.append((de_tensor_, en_tensor_))
+  return data
+
+train_data = data_process(train_filepaths)
+val_data = data_process(val_filepaths)
+test_data = data_process(test_filepaths)
 
 ### Sample Run ###
 
-sample_text = "I love machine learning"
-print(tokenize_english(sample_text))
 
-german = Field(tokenize=tokenize_german,
-               lower=True,
-               init_token="<sos>",
-               eos_token="<eos>")
-
-english = Field(tokenize=tokenize_english,
-               lower=True,
-               init_token="<sos>",
-               eos_token="<eos>")
-
-train_data, valid_data, test_data = Multi30k.splits(exts = (".de", ".en"),
-                                                    fields=(german, english))
-
-german.build_vocab(train_data, max_size=10000, min_freq=3)
-english.build_vocab(train_data, max_size=10000, min_freq=3)
-
-print(f"Unique tokens in source (de) vocabulary: {len(german.vocab)}")
-print(f"Unique tokens in target (en) vocabulary: {len(english.vocab)}")
-
-# dir(english.vocab)
-print(english.vocab.__dict__.keys())
-print(list(english.vocab.__dict__.values()))
-e = list(english.vocab.__dict__.values())
-for i in e:
-  print(i)
-  
-word_2_idx = dict(e[3])
-idx_2_word = {}
-for k,v in word_2_idx.items():
-  idx_2_word[v] = k
 
 # text cleaning preprocessing
 

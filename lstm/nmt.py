6a7ebe3c4d729fa498b5421964dfdbcb4f846125
bzolbayar <bzolbayar@ebay.com>
2022-06-18 14:34:09 -0700

    different v

--- lstm/nmt.py
+++ lstm/nmt.py
@@ -16,6 +16,48 @@ from torch.utils.tensorboard import SummaryWriter
 spacy_german = spacy.load("de")
 spacy_english = spacy.load("en")
 
+def tokenize_german(text):
+    return [token.text for token in spacy_german.tokenizer(text)]
+
+def tokenize_english(text):
+    return [token.text for token in spacy_english.tokenizer(text)]
+
+### Sample Run ###
+
+sample_text = "I love machine learning"
+print(tokenize_english(sample_text))
+
+german = Field(tokenize=tokenize_german,
+               lower=True,
+               init_token="<sos>",
+               eos_token="<eos>")
+
+english = Field(tokenize=tokenize_english,
+               lower=True,
+               init_token="<sos>",
+               eos_token="<eos>")
+
+train_data, valid_data, test_data = Multi30k.splits(exts = (".de", ".en"),
+                                                    fields=(german, english))
+
+german.build_vocab(train_data, max_size=10000, min_freq=3)
+english.build_vocab(train_data, max_size=10000, min_freq=3)
+
+print(f"Unique tokens in source (de) vocabulary: {len(german.vocab)}")
+print(f"Unique tokens in target (en) vocabulary: {len(english.vocab)}")
+
+# dir(english.vocab)
+print(english.vocab.__dict__.keys())
+print(list(english.vocab.__dict__.values()))
+e = list(english.vocab.__dict__.values())
+for i in e:
+  print(i)
+  
+word_2_idx = dict(e[3])
+idx_2_word = {}
+for k,v in word_2_idx.items():
+  idx_2_word[v] = k
+
 # text cleaning preprocessing
 
 

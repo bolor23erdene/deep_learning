943b48b2e40f77388ac0e22f9e0b8a4062e29209
bzolbayar <bzolbayar@ebay.com>
2022-06-18 14:47:58 -0700

    c

--- lstm/nmt.py
+++ lstm/nmt.py
@@ -1,20 +1,3 @@
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchtext.datasets import Multi30k
-from torchtext.data import Field, BucketIterator
-import numpy as np
-import pandas as pd
-import spacy
-import random
-from torchtext.data.metrics import bleu_score
-from pprint import pprint
-from torch.utils.tensorboard import SummaryWriter
-
-# data preparation german language
-
-
 """SPACY: open source NLP library.
 1. download the english and german language
 2. create tokenizer 

7bac9d62c5e0ce3a235f70b67a2a1443aa037475
bzolbayar <bzolbayar@ebay.com>
2022-06-20 18:50:40 -0700

    versions

--- lstm/nmt.py
+++ lstm/nmt.py
@@ -26,6 +26,8 @@ spacy.load("de_core_news_sm")
 de_tokenizer = get_tokenizer('spacy', language='de_core_news_sm')
 en_tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
 
+print(spacy.__version__, torchtext.__version__, torch.__version__)
+
 def build_vocab(filepath, tokenizer):
   counter = Counter()
   with io.open(filepath, encoding="utf8") as f:
@@ -92,11 +94,154 @@ from torch import Tensor
 
 #print(next(train_iter))
 for _, (src, trg) in enumerate(train_iter):
-    print(src,trg)
+    print(src.shape,trg.shape)
+
+
+# class Encoder:
+#     def __init__(self,):
+#         self.Embedding = torch.Embedding
+#         self.RNN = torch.GRU
+        
+        
+#     def forward():
+
+class Encoder(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 emb_dim: int,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 dropout: float):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.dropout = dropout
+
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+
+        self.rnn = nn.GRU(emb_dim, enc_hid_dim, bidirectional = True)
+
+        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
+
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self,
+                src: Tensor) -> Tuple[Tensor]:
+
+        embedded = self.dropout(self.embedding(src))
+
+        outputs, hidden = self.rnn(embedded)
+
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)))
+
+        return outputs, hidden
+    
+class Decoder(nn.Module):
+    def __init__(self,
+                 output_dim: int,
+                 emb_dim: int,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 dropout: int,
+                 attention: nn.Module):
+        super().__init__()
+
+        self.emb_dim = emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.output_dim = output_dim
+        self.dropout = dropout
+        self.attention = attention
+
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+
+        self.rnn = nn.GRU((enc_hid_dim * 2) + emb_dim, dec_hid_dim)
+
+        self.out = nn.Linear(self.attention.attn_in + emb_dim, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+
+    def _weighted_encoder_rep(self,
+                              decoder_hidden: Tensor,
+                              encoder_outputs: Tensor) -> Tensor:
+
+        a = self.attention(decoder_hidden, encoder_outputs)
+
+        a = a.unsqueeze(1)
+
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+
+        weighted_encoder_rep = torch.bmm(a, encoder_outputs)
+
+        weighted_encoder_rep = weighted_encoder_rep.permute(1, 0, 2)
+
+        return weighted_encoder_rep
+
+
+    def forward(self,
+                input: Tensor,
+                decoder_hidden: Tensor,
+                encoder_outputs: Tensor) -> Tuple[Tensor]:
+
+        input = input.unsqueeze(0)
+
+        embedded = self.dropout(self.embedding(input))
+
+        weighted_encoder_rep = self._weighted_encoder_rep(decoder_hidden,
+                                                          encoder_outputs)
+
+        rnn_input = torch.cat((embedded, weighted_encoder_rep), dim = 2)
+
+        output, decoder_hidden = self.rnn(rnn_input, decoder_hidden.unsqueeze(0))
+
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted_encoder_rep = weighted_encoder_rep.squeeze(0)
+
+        output = self.out(torch.cat((output,
+                                     weighted_encoder_rep,
+                                     embedded), dim = 1))
+
+        return output, decoder_hidden.squeeze(0)
 
 
 # text cleaning preprocessing
 
+# encoder = 
+
+
+# decoder = 
+
 
 # model setup
 # input, embeddings, encoder, decoder, dense, output
+
+### ENCODER 
+
+### DECODER 
+# for _, (src, trg) in enumerate(train_iter):
+    
+#     # src - input -> embedding layer -> lstm -> |vocab|
+    
+#     # input: batch x max_len x vocab_size
+#     # embed: batch x max_len x emd_size
+#     # lstm: 
+#     # create predictions 
+#     # create hidden
+#     #predictions = torch.zeros(n, max_len, vocab_size)
+#     for t in range(max_len):
+        
+#         lstm_input = batch x 1 x emb_size
+#         out, (hidden, cell) = LSTM(lstm_input, (hidden, cell))
+#         # hidden = batch x 1 x lstm_hidden_size
+        
+#         cur_preds = fc(hidden) # batch x 1 x lstm_hidden_size
+        
+#         predictions[prev_len:prev_len+batch_size, t, :] = cur_preds 
+        
+        
+        

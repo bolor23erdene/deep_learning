d1366e9d1019bf411f8156faa6bd371ba9153a0a
bzolbayar <bzolbayar@ebay.com>
2022-06-24 22:53:43 -0700

    change in dim

--- lstm/2.lstm_regular.py
+++ lstm/2.lstm_regular.py
@@ -65,8 +65,9 @@ def collate_batch(batch):
         offsets.append(processed_text.size(0))
     label_list = torch.tensor(label_list, dtype=torch.int64)
     offsets = torch.tensor(offsets, dtype=torch.int64)
+    text_list = torch.tensor(text_list, dtype=torch.int64)
     # pad_sequence is the magical function
-    text_list = pad_sequence(text_list, batch_first=True, padding_value=0)
+    #text_list = pad_sequence(text_list, batch_first=True, padding_value=0)
     return label_list.to(device), text_list.to(device), offsets.to(device)
 
 

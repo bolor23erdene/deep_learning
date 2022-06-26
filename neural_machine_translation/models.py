721b5d977b8d9546c9cdfd5c9e124d77e98c8bde
bzolbayar <bzolbayar@ebay.com>
2022-06-26 16:20:00 -0700

    dev in wip

--- neural_machine_translation/models.py
+++ neural_machine_translation/models.py
@@ -40,7 +40,7 @@ class Encoder(nn.Module):
     
 class Decoder(nn.Module):
     def __init__(self, de_vocab_dim, dec_hid_dim, enc_hid_dim, n_layers, bidirectional):
-    
+        super().__init__()
         self.rnn = nn.LSTM(enc_hid_dim, dec_hid_dim, n_layers, bidirectional=bidirectional, batch_first=False)
         self.fc = nn.Linear(dec_hid_dim, de_vocab_dim)
     
